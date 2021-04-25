a1a687e0806514b573c9133dfca0b2cc5f0eaf0e
vojdanristovski <62524190+vojdanristovski@users.noreply.github.com>
2021-04-25 14:19:09 +0200

    actor movie fixed

--- src/main/java/com/goldenladder/backend/repository/ActorMovieRepository.java
+++ src/main/java/com/goldenladder/backend/repository/ActorMovieRepository.java
@@ -1,11 +1,8 @@
 package com.goldenladder.backend.repository;
 
-import com.goldenladder.backend.model.Actor;
 import com.goldenladder.backend.model.ActorMovie;
 import com.goldenladder.backend.model.ActorMovieKey;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ActorMovieRepository extends JpaRepository<ActorMovie, ActorMovieKey> {
 
